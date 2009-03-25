79b06f43b8c835f51e5f635412333a34b5d463f9
babis.marmanis <babis.marmanis@12f2dfc6-82e8-11dd-b774-4f160e1b508b>
2009-03-25 01:01:37 +0000

    Removed libraries that are not used

--- trunk/deploy/bin/bsc.bat
+++ trunk/deploy/bin/bsc.bat
@@ -6,27 +6,18 @@ title Bean Shell
 set IWEB2_HOME=C:\iWeb2
 
 set LIBJARS=
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\activation.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\amazon-ecs-2007-07-16-java-library.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\blas.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\commons-codec-1.3.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\commons-httpclient-3.1.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\commons-lang-2.3.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\commons-logging-1.1.1.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\f2jutil.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jaxb-all-deps.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jaxb-api.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jaxb-impl.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jaxb-xjc.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jfreechart.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jgraph.jar
 
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jigg-0.1.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\rooster.jar
 
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\jsr173_1.0_api.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\junit-4.1.jar
-set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\lapack.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\lucene-analyzers-2.3.0.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\lucene-core-2.3.0.jar
 set LIBJARS=%LIBJARS%;%IWEB2_HOME%\deploy\lib\lucene-demos-2.3.0.jar
